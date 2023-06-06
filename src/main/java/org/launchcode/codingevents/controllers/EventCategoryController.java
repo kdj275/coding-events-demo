bde195383e32db1fb35037ccb6a3dcdd44a585c6
kelseyjungermann <kelsey.jungemrann@gmail.com>
2023-06-05 19:04:20 -0500

    finishing chapter 17 exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -3,15 +3,13 @@ package org.launchcode.codingevents.controllers;
 import org.launchcode.codingevents.data.EventCategoryRepository;
 import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
+@Controller
 @RequestMapping("eventCategories")
 public class EventCategoryController {
 
@@ -21,18 +19,18 @@ public class EventCategoryController {
     @GetMapping
     public String displayAllEvents (Model model){
         model.addAttribute("title", "All Categories");
-        model.addAttribute("categories", eventCategoryRepository.findAll())
+        model.addAttribute("categories", eventCategoryRepository.findAll());
         return "eventCategories/index";
     }
 
-    @GetMapping
+    @GetMapping ("create")
     public String renderCreateEventCategoryForm(Model model) {
         model.addAttribute("title", "Create Category");
         model.addAttribute(new EventCategory());
         return "eventCategories/create";
     }
 
-    @PostMapping
+    @PostMapping ("create")
     public String processCreateEventCategoryForm(@Valid @ModelAttribute EventCategory eventCategory, Model model, Errors errors) {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Create Category");
